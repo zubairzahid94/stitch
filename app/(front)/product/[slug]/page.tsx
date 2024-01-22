e0b4272f81f46060a496b64c7231fb43b031965b
zubairzahid94 <zubairzahid228@gmail.com>
2024-01-22 16:02:42 +0500

    7-Load-Data-fixing

--- app/(front)/product/[slug]/page.tsx
+++ app/(front)/product/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import AddToCart from "@/components/products/AddToCart";
-import data from "@/lib/data";
 import productService from "@/lib/services/productService";
+import { convertDocToObj } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -80,7 +80,12 @@ export default async function ProductDetails({
 							{product.countInStock !== 0 && (
 								<div className="card-actions justify-center">
 									<AddToCart
-										item={{ ...product, qty: 0, color: "", size: "" }}
+										item={{
+											...convertDocToObj(product),
+											qty: 0,
+											color: "",
+											size: "",
+										}}
 									/>
 								</div>
 							)}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,2 +1,8 @@
 export const round2 = (num: number) =>
 	Math.round((num + Number.EPSILON) * 100) / 100;
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function convertDocToObj(doc: any) {
+	doc._id = doc._id.toString();
+	return doc;
+}
